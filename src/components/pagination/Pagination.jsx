1b66e055c7ff339807d926f31ee9c29341561fe3
esraamagddi <esraamagdy20133@gmail.com>
2024-06-20 17:32:20 +0300

    feat:enhancing pagination style

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -15,12 +15,32 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   };
 
   return (
-    <div className="join pb-5 text-center flex gap-3 mt-5">
-      <button className="join-item btn" onClick={handlePrev} disabled={currentPage === 1}>«</button>
-      {[...Array(totalPages).keys()].map(page => (
-        <button key={page} className="join-item btn" onClick={() => onPageChange(page + 1)}>{page + 1}</button>
-      ))}
-      <button className="join-item btn" onClick={handleNext} disabled={currentPage === totalPages}>»</button>
+    <div className="flex justify-center mt-5">
+      <div className="inline-flex shadow-md rounded-lg">
+        <button 
+          className={`px-3 py-1 rounded-l-md border border-gray-300 ${currentPage === 1 ? 'bg-gray-200 text-gray-500 cursor-not-allowed' : 'bg-white text-blue-600 hover:bg-blue-100'}`} 
+          onClick={handlePrev} 
+          disabled={currentPage === 1}
+        >
+          «
+        </button>
+        {[...Array(totalPages).keys()].map(page => (
+          <button 
+            key={page} 
+            className={`px-3 py-1 border-t border-b border-gray-300 ${currentPage === page + 1 ? 'bg-blue-500 text-white' : 'bg-white text-blue-600 hover:bg-blue-100'}`} 
+            onClick={() => onPageChange(page + 1)}
+          >
+            {page + 1}
+          </button>
+        ))}
+        <button 
+          className={`px-3 py-1 rounded-r-md border border-gray-300 ${currentPage === totalPages ? 'bg-gray-200 text-gray-500 cursor-not-allowed' : 'bg-white text-blue-600 hover:bg-blue-100'}`} 
+          onClick={handleNext} 
+          disabled={currentPage === totalPages}
+        >
+          »
+        </button>
+      </div>
     </div>
   );
 };
