748e1e3f057b0b8cd351fb952f79e6902ad184fd
esraamagddi <esraamagdy20133@gmail.com>
2024-06-20 11:04:52 +0300

    feat:add delete project functionality

--- src/components/project/ProjectDetails.jsx
+++ src/components/project/ProjectDetails.jsx
@@ -1,8 +1,6 @@
-// ProjectDetails.jsx
-
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom'; // Importing useParams for accessing route parameters
+import { useParams } from 'react-router-dom'; 
 import { fetchProjectById, deleteProject ,setSelectedProject} from '../../store/slices/projectSlice';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,7 +10,7 @@ const ProjectDetails = () => {
   const project = useSelector(state => state.projects.selectedProject);
   const loading = useSelector(state => state.projects.loading);
   const error = useSelector(state => state.projects.error);
-  const navigate = useNavigate(); // Use useNavigate hook here
+  const navigate = useNavigate(); 
 
   useEffect(() => {
     dispatch(fetchProjectById(projectId));
@@ -26,10 +24,9 @@ const ProjectDetails = () => {
     if (window.confirm("Are you sure you want to delete this project?")) {
       try {
         await dispatch(deleteProject(projectId));
-        navigate('/projects'); // Navigate to '/projects' route after deletion
+        navigate('/projects'); 
       } catch (error) {
         console.error("Error deleting project:", error);
-        // Handle error as needed
       }
     }
   };
