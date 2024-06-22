d3f323e86a437379c6956e7477f51f074000489d
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-06-22 12:04:47 +0300

    fix: pagination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -18,7 +18,7 @@ export const unsubscribeSource = (sourceId) => {
     });
 };
 
-  export const getTopFiveSources = () => {
+export const getTopFiveSources = () => {
   return axiosInstance.get('/sources/topFive')
     .then(response => {
         return response.data;
@@ -50,34 +50,3 @@ export const getSubscribedArticles = (page = 1, pageSize = 10) => {
       throw error;
     });
 };
-
-
-return (
-  <div className="flex justify-center mt-5">
-    <div className="inline-flex shadow-md rounded-lg">
-      <button 
-        className={`px-3 py-1 rounded-l-md border border-gray-300 ${currentPage === 1 ? 'bg-gray-200 text-gray-500 cursor-not-allowed' : 'bg-white text-blue-600 hover:bg-blue-100'}`} 
-        onClick={handlePrev} 
-        disabled={currentPage === 1}
-      >
-        «
-      </button>
-      {[...Array(totalPages).keys()].map(page => (
-        <button 
-          key={page} 
-          className={`px-3 py-1 border-t border-b border-gray-300 ${currentPage === page + 1 ? 'bg-blue-500 text-white' : 'bg-white text-blue-600 hover:bg-blue-100'}`} 
-          onClick={() => onPageChange(page + 1)}
-        >
-          {page + 1}
-        </button>
-      ))}
-      <button 
-        className={`px-3 py-1 rounded-r-md border border-gray-300 ${currentPage === totalPages ? 'bg-gray-200 text-gray-500 cursor-not-allowed' : 'bg-white text-blue-600 hover:bg-blue-100'}`} 
-        onClick={handleNext} 
-        disabled={currentPage === totalPages}
-      >
-        »
-      </button>
-    </div>
-  </div>
-);
