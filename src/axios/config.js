0767bc33b71e25fe8086d2a18258e7b479b54dcd
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-06-22 12:24:17 +0300

    fix: base url for axios instance

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 axiosInstance.interceptors.request.use(
