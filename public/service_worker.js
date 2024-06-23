32518b59f60184fd1296b6561e2bff5fc2d7a0cd
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-06-24 00:01:31 +0300

    fix: navbar icons appearance

--- public/service_worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('push', event => {
-  const data = event.data.json();
-  self.registration.showNotification(data.notification.title, {
-    body: data.notification.body,
-    icon: data.notification.icon
-  });
-});
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -115,7 +115,57 @@ const Navbar = () => {
             ) : (
               <>
 
-                <Dropdown>
+                    <li className="nav-item dropdown no-arrow mx-1">
+                          <a className="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
+                              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              <i className="fas fa-bell fa-2x"></i>
+                              
+                              <span className="badge badge-danger badge-counter">3</span>
+                          </a>
+                          
+                          <div className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                              aria-labelledby="alertsDropdown">
+                              <h6 className="dropdown-header">
+                                  Alerts Center
+                              </h6>
+                              <a className="dropdown-item d-flex align-items-center" href="#">
+                                  <div className="mr-3">
+                                      <div className="icon-circle bg-primary">
+                                          <i className="fas fa-file-alt text-white"></i>
+                                      </div>
+                                  </div>
+                                  <div>
+                                      <div className="small text-gray-500">December 12, 2019</div>
+                                      <span className="font-weight-bold">A new monthly report is ready to download!</span>
+                                  </div>
+                              </a>
+                              <a className="dropdown-item d-flex align-items-center" href="#">
+                                  <div className="mr-3">
+                                      <div className="icon-circle bg-success">
+                                          <i className="fas fa-donate text-white"></i>
+                                      </div>
+                                  </div>
+                                  <div>
+                                      <div className="small text-gray-500">December 7, 2019</div>
+                                      $290.29 has been deposited into your account!
+                                  </div>
+                              </a>
+                              <a className="dropdown-item d-flex align-items-center" href="#">
+                                  <div className="mr-3">
+                                      <div className="icon-circle bg-warning">
+                                          <i className="fas fa-exclamation-triangle text-white"></i>
+                                      </div>
+                                  </div>
+                                  <div>
+                                      <div className="small text-gray-500">December 2, 2019</div>
+                                      Spending Alert: We've noticed unusually high spending for your account.
+                                  </div>
+                              </a>
+                              <a className="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                          </div>
+                      </li>
+                
+                {/* <Dropdown>
                   <Dropdown.Toggle variant="outline-primary" id="dropdown-basic">
                      اشعارات
                   </Dropdown.Toggle>
@@ -130,14 +180,12 @@ const Navbar = () => {
                         <Dropdown.Item> No Notifications Yet! </Dropdown.Item>
                     </Dropdown.Menu>
                     )}
-                </Dropdown>
+                </Dropdown> */}
 
-                <li className="nav-item">
-                  <button className="btn btn-outline-primary" onClick={logOut}>تسجيل خروج</button>
-                </li>
+                
                 <li className="nav-item dropdown">
                   <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i className="fa fa-user"></i>
+                    <i className="fa fa-user fa-2x"></i>
                   </a>
                   <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
                     <li><NavLink className="dropdown-item" to="/profile">Profile</NavLink></li>
@@ -145,6 +193,10 @@ const Navbar = () => {
                     <li><hr className="dropdown-divider" /></li>
                     <li><button className="dropdown-item" onClick={logOut}>Logout</button></li>
                   </ul>
+                </li>
+
+                <li className="nav-item">
+                  <button className="btn btn-outline-primary" onClick={logOut}>تسجيل خروج</button>
                 </li>
               </>
             )}
