ead606ec95ae16e465c71e2daceb5cc709751d56
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-06-23 18:51:29 +0300

    fix: bugs

--- src/axios/notifications.js
+++ src/axios/notifications.js
@@ -1,40 +1,13 @@
 import axiosInstance from "./config";
 
-export const createDummyProject = () => {
-  return axiosInstance.post(
-    "/projects/",
-    {
-        "client": {
-            "user": "65b84e2b0579b171776d0aec",
-            "fullName": "alik",
-            "mobileNumber": "888-999-0000"
-        },
-        "name": "abdo opooooooooooooooooooooo",
-        "number": 12,
-        "description": "T project."
-    }
-  );
-};
-
-export const subscribeNotification = (subscription) => {
-  return axiosInstance.post(
-    "/subscribe/", JSON.stringify(subscription),
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-};
-
-export const sendRealNotifications = () => {
-  return axiosInstance.post( "/sendNotification/" )
-};
-
+// localStorage.getItem('token')
+// const decodedToken = jwt.verify(localStorage.getItem('token'), process.env.JWT_SECRET);
+// console.log(decodedToken);
 
 export const getMyNotifications = () => {
   return axiosInstance.get(
-    "/project-notification/user/6676b8039dec5a7d4fd7ad90",
+    // `/project-notification/user/${decodedToken.id}`,
+    `/project-notification/user/6676b8039dec5a7d4fd7ad90`,
     {
       headers: {
         "Content-Type": "application/json",
