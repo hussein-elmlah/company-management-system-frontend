df3b5f7fc94508375f06acd505269f3dbb307c7b
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-06-25 19:41:47 +0300

    feat: stick footer to the bottom

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <footer className="footer footer-center p-3 m-0 bg-base-200 bg-dark text-white text-center">
+    
+    <footer className="footer footer-center p-3 m-0 bg-base-200 bg-dark text-white text-center custom_footer">
         <p className="font-bold m-0">
           ITI developers. <br />
           Providing reliable web designs.
