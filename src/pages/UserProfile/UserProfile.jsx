a3dd2485d6822e474f4181b5e0f058730cdc6920
hussein-elmlah <husseinelmlah11@gmail.com>
2024-06-27 07:22:32 +0300

    add user profile page

---
+++ src/pages/UserProfile/UserProfile.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../store/slices/userSlice';
+import { QueueSpinner } from "../../components/reusables/LoadingSpinner";
+
+const UserProfile = () => {
+  const user = useSelector(selectUser);
+
+  if (!user) {
+    return <QueueSpinner />;
+  }
+
+  return (
+    <div className="container">
+      <h2 className="mb-4">User Profile</h2>
+      <div className="card">
+        <div className="row card-body">
+          <h5 className="card-title">Profile Details</h5>
+          <p className="card-text"><strong>Username:</strong> {user.username}</p>
+          <p className="card-text"><strong>Role:</strong> {user.role}</p>
+          <p className="card-text"><strong>Department:</strong> {user.department ? user.department.name : 'N/A'}</p>
+          <p className="card-text"><strong>First Name:</strong> {user.firstName}</p>
+          <p className="card-text"><strong>Last Name:</strong> {user.lastName}</p>
+          <p className="card-text"><strong>Date of Birth:</strong> {user.dateOfBirth ? new Date(user.dateOfBirth).toLocaleDateString() : 'N/A'}</p>
+          <p className="card-text"><strong>Address:</strong> {user.address}</p>
+          <p className="card-text"><strong>Job Level:</strong> {user.jobLevel}</p>
+          <p className="card-text"><strong>Mobile Number:</strong> {user.mobileNumber}</p>
+          <p className="card-text"><strong>Email:</strong> {user.email}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
